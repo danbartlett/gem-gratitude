754e6a861a3b53eab5175d6a7a49f138d45ebc36
Dan Bartlett <danbartlett@gmail.com>
2014-11-29 19:45:31 +0000

    Move HTML to .erb and get accordion working

--- gem-gratitude.rb
+++ gem-gratitude.rb
@@ -1,12 +1,13 @@
 require 'rubygems'
 require 'httparty'
 require 'json'
+require 'erb'
 
 # Load Gemfile
 gemfile_path=File.expand_path('../Gemfile', __FILE__)
 
 # List of all gems
-gem_list = []
+@gem_list = []
 
 # Characters to remove to get the pure gem name
 replacements = [ '\'', '"', ',' ]
@@ -19,12 +20,12 @@ File.open(gemfile_path).read.each_line do |line|
 		replacements.each {|r| gem_name.gsub!(r, '')}
 		next if parts[2] == 'github:'
     gem_spec = Gem::Specification.find_by_name(gem_name)
-	 	gem_list << {name: gem_name, homepage: gem_spec.homepage}
+	 	@gem_list << {name: gem_name, homepage: gem_spec.homepage}
 	end
 end
 
 # Not every gem_spec.homepage is a GitHub repo; try and find a GitHub link
-gem_list.each do
+@gem_list.each do
 	# if g[:homepage] =~ /https:\/\/github.com\//
 end
 
@@ -32,28 +33,20 @@ end
 file = File.open('giveback.html', 'w')
 
 # Find all open issues via the GitHub API
-html_content = ''
-issue_count = 0
-gem_list[0..5].each do |g|
+@html_content = ''
+@issue_count = 0
+@gem_list[0..5].each do |g|
 	github_url = g[:homepage].split('/')
 	response = HTTParty.get("https://api.github.com/repos/#{github_url[-2]}/#{github_url[-1]}/issues?state=open")
 	json = JSON.parse(response.body)
 	if response.code == 200
 		puts "#{g[:name]}: #{g[:homepage]} - #{json.count} open issues!"
 		json.each do |issue|
-			issue_count += 1
-			html_content << "<h3>#{g[:name]}: <a href=\"#{issue['html_url']}\">#{issue['title']}</a></h3><div>#{issue['body'].gsub!(/(?:\n\r?|\r\n?)/, '<br>')}</div></h3>"
+			@issue_count += 1
+			@html_content << "<h3>#{g[:name]}: <a href=\"#{issue['html_url']}\">#{issue['title']}</a></h3><div>#{issue['body'].gsub!(/(?:\n\r?|\r\n?)/, '<br>')}</div></h3>"
 		end
 	end
 end
 
-file.write '<script src="http://code.jquery.com/jquery-1.10.2.js"></script><script src="http://code.jquery.com/ui/1.11.2/jquery-ui.js"></script>'
-file.write '<script>$(function() { $( "#accordion" ).accordion(); }); </script>'
-file.write '<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">'
-file.write "<strong>Found #{issue_count} open issues across #{gem_list.count} gems you depend on. Happy hacking!</strong>"
-
-# Write all the content to a HTML file
-file.write "<div id='accordian'>"
-file.write html_content
-file.write "</div>"
-file.write "</html>"
+erb = ERB.new(File.read('template.erb'))
+file.write erb.result(binding)
